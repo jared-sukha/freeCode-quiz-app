c1da11418d579655665d253a16822251cac98f1a
jared-sukha <jared.sukha@gmail.com>
2022-09-11 08:26:02 +1200

    updated local main branch

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,61 +1,99 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { getTheQuotes } from './ApiClient'
 
 export default function App() {
+  //  const questions = [
+  //   {
+  //     questionText: 'What is the capital of France?',
+  //     answerOptions: [
+  //       { answerText: 'New York', isCorrect: false },
+  //       { answerText: 'London', isCorrect: false },
+  //       { answerText: 'Paris', isCorrect: true },
+  //       { answerText: 'Dublin', isCorrect: false },
+  //     ],
+  //   },
+  //   {
+  //     questionText: 'Who is CEO of Tesla?',
+  //     answerOptions: [
+  //       { answerText: 'Jeff Bezos', isCorrect: false },
+  //       { answerText: 'Elon Musk', isCorrect: true },
+  //       { answerText: 'Bill Gates', isCorrect: false },
+  //       { answerText: 'Tony Stark', isCorrect: false },
+  //     ],
+  //   },
+  //   {
+  //     questionText: 'The iPhone was created by which company?',
+  //     answerOptions: [
+  //       { answerText: 'Apple', isCorrect: true },
+  //       { answerText: 'Intel', isCorrect: false },
+  //       { answerText: 'Amazon', isCorrect: false },
+  //       { answerText: 'Microsoft', isCorrect: false },
+  //     ],
+  //   },
+  //   {
+  //     questionText: 'How many Harry Potter books are there?',
+  //     answerOptions: [
+  //       { answerText: '1', isCorrect: false },
+  //       { answerText: '4', isCorrect: false },
+  //       { answerText: '6', isCorrect: false },
+  //       { answerText: '7', isCorrect: true },
+  //     ],
+  //   },
+  // ]
 
-   const questions = [
+  // TODO
+  // Will need useEffect to render inital random quotes (logic defined in ApiClient.js)
+  // Also need to decide how answers will work, Answers will return random string array of character names (including answer) so will need to refer to questions and their character answers, to make sure answer is included in answer array.
+
+  const [questions, setQuestions] = useState([
     {
-      questionText: 'What is the capital of France?',
-      answerOptions: [
-        { answerText: 'New York', isCorrect: false },
-        { answerText: 'London', isCorrect: false },
-        { answerText: 'Paris', isCorrect: true },
-        { answerText: 'Dublin', isCorrect: false },
-      ],
+      quote:
+        "This stuff'll make you a goddamn sexual tyrannosaurus... just like me.",
+      character: 'Blain',
     },
     {
-      questionText: 'Who is CEO of Tesla?',
-      answerOptions: [
-        { answerText: 'Jeff Bezos', isCorrect: false },
-        { answerText: 'Elon Musk', isCorrect: true },
-        { answerText: 'Bill Gates', isCorrect: false },
-        { answerText: 'Tony Stark', isCorrect: false },
-      ],
+      quote: 'Payback time.',
+      character: 'Blain',
     },
     {
-      questionText: 'The iPhone was created by which company?',
-      answerOptions: [
-        { answerText: 'Apple', isCorrect: true },
-        { answerText: 'Intel', isCorrect: false },
-        { answerText: 'Amazon', isCorrect: false },
-        { answerText: 'Microsoft', isCorrect: false },
-      ],
+      quote: "You lose it here, you're in a world of hurt.",
+      character: 'Blain',
     },
     {
-      questionText: 'How many Harry Potter books are there?',
-      answerOptions: [
-        { answerText: '1', isCorrect: false },
-        { answerText: '4', isCorrect: false },
-        { answerText: '6', isCorrect: false },
-        { answerText: '7', isCorrect: true },
-      ],
+      quote: 'Makes Cambodia look like Kansas.',
+      character: 'Blain',
     },
+  ])
+
+  const answers = [
+    'Dutch', 'Dillon', 'Blain', 'Billy'
   ]
 
   const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [showScore, setShowScore] = useState(false);
-  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false)
+  const [score, setScore] = useState(0)
 
-  const handleAnswerClick = (isCorrect) =>{
-    const nextQuestion = currentQuestion + 1;
+  const handleAnswerClick = (isCorrect) => {
+    const nextQuestion = currentQuestion + 1
+    // getTheQuotes()
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion)
+    } else {
+      setShowScore(true)
+    }
     if (isCorrect) {
       setScore(score + 1)
     }
-    if(nextQuestion < questions.length){
-      setCurrentQuestion(nextQuestion)
-    } else{
-      setShowScore(true)
-    }
   }
+
+  // const handleNextQuestionClick = () => {
+  //   const nextQuestion = currentQuestion + 1;
+  //   if(nextQuestion < questions.length){
+  //     setCurrentQuestion(nextQuestion)
+  //   } else{
+  //     setShowScore(true)
+  //   }
+  // }
 
   return (
     <div className="app">
@@ -69,15 +107,25 @@ export default function App() {
         <>
           <div className="question-section">
             <div className="question-count">
-              <span>Question {currentQuestion+1}</span>/{questions.length}
+              <span>Who said it?</span>
+              <span>
+                {' '}
+                {currentQuestion + 1}/{questions.length}
+              </span>
             </div>
-            <div className="question-text">{questions[currentQuestion].questionText}</div>
+            <div className="question-text">
+              {questions[currentQuestion].quote}
+            </div>
           </div>
           <div className="answer-section">
-            {questions[currentQuestion].answerOptions.map((answer, i) => (
-              <button onClick={()=>handleAnswerClick(answer.isCorrect)}>{answer.answerText}</button>
+            {answers.map((answer, i) => (
+              <button key={i} onClick={()=>handleAnswerClick(answer===questions[currentQuestion].character)}>{answer} </button>
             ))}
+            
           </div>
+          {/* <div className="next-question-section">
+          <button onClick={()=>handleNextQuestionClick()}className="next-button">Next Question</button>
+          </div> */}
         </>
       )}
     </div>
